972f69edaf43564fb75a6bc061aba1e8820bbc4b
sunhf <sunhf01@jcgroup.com.cn>
2018-06-13 13:59:03 +0800

    feat: init entry

---
+++ packages/loading/src/index.vue
@@ -0,0 +1,14 @@
+ <template>
+  <div class="loading" :class="className">
+    <section class="loading-content">
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    className: { type: String, default: '' },
+  },
+}
+</script>
